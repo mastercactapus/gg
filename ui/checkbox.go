13315347b46ee8064f46c7253f753f644686a67f
Nathan Caza <mastercactapus@gmail.com>
2017-06-05 23:46:22 -0500

    add checkbox

---
+++ ui/checkbox.go
@@ -0,0 +1,60 @@
+package ui
+
+import termbox "github.com/nsf/termbox-go"
+
+const (
+	radioChecked   = "◈"
+	radioUnchecked = "◇"
+
+	checked   = "☑"
+	unchecked = "☐"
+)
+
+type Checkbox struct {
+	X, Y        int
+	Width       int
+	Text        string
+	Enabled     bool
+	Checked     bool
+	Radio       bool
+	OnClickFunc func(x, y int, newState bool)
+}
+
+func (c *Checkbox) OnClick(x, y int) {
+	if !c.Enabled {
+		return
+	}
+	if c.OnClickFunc == nil {
+		return
+	}
+	c.OnClickFunc(x, y, !c.Checked)
+}
+func (c *Checkbox) Draw(r Renderer) {
+	w := c.Width
+	minW := len(c.Text) + 2
+	if minW > w {
+		w = minW
+	}
+	sw, sh := r.Size()
+	x, y, w, _ := StandardSize(c.X, c.Y, w, 1, sw, sh)
+
+	var icon string
+	switch {
+	case c.Checked && c.Radio:
+		icon = radioChecked
+	case c.Checked && !c.Radio:
+		icon = checked
+	case !c.Checked && c.Radio:
+		icon = radioUnchecked
+	case !c.Checked && !c.Radio:
+		icon = radioChecked
+	}
+
+	var fg, bg termbox.Attribute
+	if !c.Enabled {
+		fg = termbox.ColorBlack
+	}
+
+	rs := []rune(icon + " " + c.Text)
+	putRunesA(r, x, y, rs, fg, bg)
+}
