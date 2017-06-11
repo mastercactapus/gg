97f2d95c25006b711a1827150d772fbd08a48352
Nathan Caza <mastercactapus@gmail.com>
2017-06-11 11:39:09 -0500

    support colors in text

--- ui/text.go
+++ ui/text.go
@@ -1,10 +1,15 @@
 package ui
 
-import "strings"
+import (
+	"strings"
+
+	termbox "github.com/nsf/termbox-go"
+)
 
 type Text struct {
-	X, Y  int
-	Lines []string
+	X, Y   int
+	FG, BG termbox.Attribute
+	Lines  []string
 }
 
 func (t Text) Draw(r Renderer) {
@@ -12,6 +17,6 @@ func (t Text) Draw(r Renderer) {
 	space := strings.Repeat(" ", w)
 	for i, l := range t.Lines {
 		rs := []rune(l + space)
-		putRunes(r, t.X, t.Y+i, rs[:w])
+		putRunesA(r, t.X, t.Y+i, rs[:w], t.FG, t.BG)
 	}
 }
